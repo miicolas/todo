6be41dc0cd9bf881e216b04027106bd83810e0d5
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-02-17 17:11:58 +0100

    add select button sidebar

--- src/Layout/layout.jsx
+++ src/Layout/layout.jsx
@@ -1,19 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home.jsx";
-import About from "../pages/about.jsx";
-import Contact from "../pages/contact.jsx";
+import SideBar from "../Components/SideBar/sidebar.jsx";
 
 
 function Layout (){
     return (
-        <div className="">
-            Header 
+        <div className="flex">
+            <SideBar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
             </Routes>
-            Footer
         </div>
 
     )
